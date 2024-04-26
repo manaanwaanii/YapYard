5eda9e7c55c07c9dbab228311d067aad250a794e
Manaan Waani <manaanwaani@outlook.com>
2024-04-26 23:08:13 +0530

    Initial commit

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,12 @@
+//url routes for authentication
+import { login, signup, logout } from "./controllers/auth.controllers.js";
+import express from "express";
+
+const router = express.Router();
+
+router.post("/signup",signup);
+
+router.post("/login",login);
+
+router.post("/logout",logout);
+export default router
---
+++ backend/server.js
@@ -0,0 +1,17 @@
+
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+app.get("/",(req, res) => {
+    // root route : http://localhost:8000/
+    res.send("Server is  Ready")
+});
+
+app.use("/api/auth",authRoutes)
+
+app.listen(PORT,() => console.log(`Server Running on Port ${PORT}`));
