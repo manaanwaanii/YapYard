7c2a6e02e3d88772714e2f62c8ee43e26354208c
Manaan Waani <manaanwaani@outlook.com>
2024-05-06 10:39:54 +0530

    Passwords Hashed and login Function

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,5 +1,9 @@
 //Individual Functions for Authentication
-import User from "../models/user.model.js";
+import User from "../models/user.model.js"; 
+import bcryptjs from "bcryptjs"
+import generateTokenAndSetCookie from "../utils/generateToken.js";
+
+
 export const signup = async (req,res) => {
     try {
         const {fullName, username ,password, confirmPassword, gender} = req.body;
@@ -15,6 +19,9 @@ export const signup = async (req,res) => {
         }
 
     // Password Hash here
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
     // https://avatar-placeholder.iran.liara.run/
 
     const boyProfilePic =  `https://avatar.iran.liara.run/public/boy?username=${username}`
@@ -23,28 +30,55 @@ export const signup = async (req,res) => {
     const newUser = new User({
         fullName,
         username,
-        password,
+        password:hashedPassword,
         gender,
         profilePic: gender === "male" ? boyProfilePic : girlProfilePic
     })
 
-    await newUser.save();
-    res.status(201).json({
-        _id: newUser. _id,
-        fullName: newUser.fullName,
-        username: newUser.username,
-        profilePic: newUser.profilePic,
-    })
+    if(newUser){
+        //Generate JWT token here
+        generateTokenAndSetCookie(newUser._id, res);
+        await newUser.save();
+
+        res.status(201).json({
+            _id: newUser. _id,
+            fullName: newUser.fullName,
+            username: newUser.username,
+            profilePic: newUser.profilePic,
+        });
+    }else{
+        res.status(400).json({error : "Invalid Data"});
+    }
     } catch (error) {
         console.log("Error in signup controller", error.message);
         res.status(500).json({error:"Server Error"});
     }
 };
 
-export const login = (req,res) =>{
-    console.log("loginUser");
-};
+export const login = async (req,res) =>{
+    try {
+        const {username, password} = req.body;
+        const user = await User.findOne({username});
+        const isPasswordCorrect = await bcryptjs.compare(password, user?.password || "");
 
+        if(!user || !isPasswordCorrect) {
+            return res.status(400).json({error: "Invalid credentials"});
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            profilePic: user.profilePic,
+        });
+
+    }   catch(error){
+            console.log("Error in login controller", error.message)
+        res.status(500).json({ error: "Internal Server Error"})    
+        }
+    }
 export const logout = (req,res) =>{
     console.log("logoutUser");
 };
