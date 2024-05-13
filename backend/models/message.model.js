57bc68c806bb39502262eabdee96eff32d878c8b
Manaan Waani <manaanwaani@outlook.com>
2024-05-13 14:33:30 +0530

    Finalising Messages And Conversations With API testings

--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -1,22 +1,26 @@
 import mongoose from "mongoose";
-const messageSchema = newmongoose.messageSchema({
-    senderId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    },
-    receiverId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    },
-    message:{
-        type: String,
-        required: true
-    },
-    //Gives the Created and Updated at fields
-}, {timestammps: true});
 
-const Message = mongoose.model("Message",messageSchema);
+const messageSchema = new mongoose.Schema(
+	{
+		senderId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		receiverId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		message: {
+			type: String,
+			required: true,
+		},
+		// createdAt, updatedAt
+	},
+	{ timestamps: true }
+);
+
+const Message = mongoose.model("Message", messageSchema);
 
 export default Message;
