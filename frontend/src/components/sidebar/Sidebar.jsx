ea775b9fbed219514ef0ca8ae5f062bddefdd5a2
Manaan Waani <manaanwaani@outlook.com>
2024-05-17 14:15:57 +0530

    SideBar Finalised

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,11 +1,12 @@
+  import Conversations from "./Conversations";
   import SearchInput from "./SearchInput" ;
    const Sidebar = () => {
    	return (
 		<div>
    			<SearchInput />
    			<div className='divider px-3'></div>
-   			{/*<Conversations />
-   			<LogoutButton/> */}
+			<Conversations/>
+   			<LogoutButton/>
    		</div>
    	);
    };
