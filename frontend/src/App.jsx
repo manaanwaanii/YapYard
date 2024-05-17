805670f0f4da82735d3dca7178dca46a04fa0d66
Manaan Waani <manaanwaani@outlook.com>
2024-05-17 11:32:03 +0530

    Bug Fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
+import Home from "./pages/home/Home"
+import Login from "./pages/login/Login"
+import Signup from "./pages/signup/Signup"
 
 function App() {
-  return <div className= 'p-4 h-screen flex items-center justify-center'>
-      <Login/>
+  return(
+    <div className= 'p-4 h-screen flex items-center justify-center'>
+      {/* <Signup /> */}
+      <Home />
   </div>
+  ) 
 }
 
 export default App
--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,14 +1,11 @@
-   import Conversations from "./Conversations";
-   import LogoutButton from "./LogoutButton";
-   import SearchInput from "./SearchInput";
-
+  import SearchInput from "./SearchInput" ;
    const Sidebar = () => {
    	return (
-   		<div className='border-r border-slate-500 p-4 flex flex-col'>
+		<div>
    			<SearchInput />
    			<div className='divider px-3'></div>
-   			<Conversations />
-   			<LogoutButton />
+   			{/*<Conversations />
+   			<LogoutButton/> */}
    		</div>
    	);
    };
