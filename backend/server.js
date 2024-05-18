a125dd18427fc9a98b2811c9bdd5e784f2ae8efe
Manaan Waani <manaanwaani@outlook.com>
2024-05-18 22:20:42 +0530

    rebuild

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import path from "path"
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -7,8 +7,8 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
-import { app,server } from "./socket/socket.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
@@ -16,20 +16,20 @@ const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json()); 
 app.use(cookieParser());
 
-app.use("/api/auth",authRoutes)
-app.use("/api/messages",messageRoutes)
-app.use("/api/users", userRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*",(req,res) => {
-    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
-})
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
-server.listen(PORT,() => {
-    connectToMongoDB()
-    console.log(`Server Running on Port ${PORT}`)
+server.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
 });
