eedf08b5625da7a3b0450ab6bc2bac002b8b3af5
Manaan Waani <manaanwaani@outlook.com>
2024-05-18 17:14:55 +0530

    Messages folder Relocated

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,9 +1,9 @@
 import { useEffect } from "react";
-import useConversation from "../../../zustand/useConversation";
+import useConversation from "../../zustand/useConversation";
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
 import { TiMessages } from "react-icons/ti";
-import { useAuthContext } from "../../../context/AuthContext";
+import { useAuthContext } from "../../context/AuthContext";
 const MessageContainer = () => {
 	const { selectedConversation, setSelectedConversation } = useConversation();
 
