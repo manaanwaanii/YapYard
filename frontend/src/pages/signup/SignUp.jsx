1a63db6925d0976deec00b3aeb7b69f3e27deff1
Manaan Waani <manaanwaani@outlook.com>
2024-05-18 12:00:44 +0530

    SignUp Hook Finalised

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -87,9 +87,11 @@ import useSignup from "../../hooks/useSignup";
 						Already have an account?
 					</Link>
 
-  					<div>
-  						<button className='btn btn-block btn-sm mt-2 border border-slate-700'>Sign Up</button>
-  					</div>
+					<div>
+						<button className='btn btn-block btn-sm mt-2 border border-slate-700' disabled={loading}>
+							{loading ? <span className='loading loading-spinner'></span> : "Sign Up"}
+						</button>
+					</div>
   				</form>
   			</div>
   		</div>
