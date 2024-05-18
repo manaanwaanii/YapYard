eba3978cc31b3b99ad1bfb4fc51ec2368e522c14
Manaan Waani <manaanwaani@outlook.com>
2024-05-18 21:38:55 +0530

    Finalising Build | Pre-deployment

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
+import path from "path"
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
+
 import { app,server } from "./socket/socket.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
 dotenv.config();
 
 app.use(express.json());
@@ -18,11 +22,11 @@ app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users", userRoutes)
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
-// app.get("/",(req, res) => {
-//     // root route : http://localhost:5000/
-//     res.send("Server is  Ready")
-// });
+app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 server.listen(PORT,() => {
     connectToMongoDB()
