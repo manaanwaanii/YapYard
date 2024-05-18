8a300872293a2cf9cbf22170f7535cef206d4101
Manaan Waani <manaanwaani@outlook.com>
2024-05-18 17:45:57 +0530

    Pre-Socket Finalization

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import Sidebar from "../../components/sidebar/Sidebar"
-import MessageContainer from "../../components/sidebar/messages/MessageContainer";
+import MessageContainer from "../../components/messages/MessageContainer";
 const Home = () => {
 	return <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
 			<Sidebar />
