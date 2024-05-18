ccf343998118718cb13b00736ca3f190540119f3
Manaan Waani <manaanwaani@outlook.com>
2024-05-18 17:54:06 +0530

    Post-Socket Backend Finalization

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,9 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
-
+import { app,server } from "./socket/socket.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -25,7 +24,7 @@ app.use("/api/users", userRoutes)
 //     res.send("Server is  Ready")
 // });
 
-app.listen(PORT,() => {
+server.listen(PORT,() => {
     connectToMongoDB()
     console.log(`Server Running on Port ${PORT}`)
 });
